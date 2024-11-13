68b882e2fe71ed68e059e2d7ad5ba37aea8de3b2
Maiya <maiyajanay@gmail.com>
2024-11-12 20:02:33 -0500

    added styling and created details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -10,12 +12,12 @@ import { HomeComponent } from './home/home.component';
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
       </main>
   `,
   styleUrls: ['./app.component.css'],
-  imports: [HomeComponent]
+  imports: [HomeComponent, HousingLocationComponent, RouterModule]
 })
 export class AppComponent {
   title = 'homes';
